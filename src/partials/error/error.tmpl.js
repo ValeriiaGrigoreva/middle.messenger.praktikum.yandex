20a42bada04c1cf174786625a783a623a3f734b5
Валерия Григорьева <grigorevavm@intech.rshb.ru>
2023-06-09 22:45:27 +0300

    Sprint 1

---
+++ src/partials/error/error.tmpl.js
@@ -0,0 +1,9 @@
+import './error.scss'
+
+export default `
+    <p class="error-number">{{number}}</p>
+    <p class="error-text">{{text}}</p>
+    <a href={{link}}>
+        <p class="error-back">Назад к чатам</p>
+    </a>
+`
